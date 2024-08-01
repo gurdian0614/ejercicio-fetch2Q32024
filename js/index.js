ca60bb662929244fd2a7d048152c0416d445359f
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-01 06:06:10 -0600

    Ajuste de tarjeta de usuarios

--- js/index.js
+++ js/index.js
@@ -5,26 +5,24 @@ fetch("https://jsonplaceholder.typicode.com/users")
     
     for (let usuario of data) {
         card += `<div class="col">
-                        <div class="card">
-                        <div class="card h-100">
-                            <img src="https://www.cavsi.com/espanol/wp-content/uploads/informatica/usuario-de-computadoras.jpg" class="card-img-top" alt="Usuario">
-                            <div class="card-body">
-                                <h5 class="card-title">${usuario.name}</h5>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item"><strong>Nombre de usuario:</strong> ${usuario.username}</li>
-                                    <li class="list-group-item"><strong>Correo electrónico:</strong> ${usuario.email}</li>
-                                    <li class="list-group-item"><strong>Calle:</strong> ${usuario.address.street}</li>
-                                    <li class="list-group-item"><strong>Ciudad:</strong> ${usuario.address.city}</li>
-                                    <li class="list-group-item"><strong>Teléfono:</strong> ${usuario.phone}</li>
-                                    <li class="list-group-item"><strong>Compañía:</strong> ${usuario.company.name}</li>
-                                </ul>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">${usuario.website}</small>
-                            </div>
+                    <div class="card">
+                        <img src="https://www.cavsi.com/espanol/wp-content/uploads/informatica/usuario-de-computadoras.jpg" class="card-img-top" alt="Usuario">
+                        <div class="card-body">
+                            <h5 class="card-title">${usuario.name}</h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"><strong>Nombre de usuario:</strong> ${usuario.username}</li>
+                                <li class="list-group-item"><strong>Correo electrónico:</strong> ${usuario.email}</li>
+                                <li class="list-group-item"><strong>Calle:</strong> ${usuario.address.street}</li>
+                                <li class="list-group-item"><strong>Ciudad:</strong> ${usuario.address.city}</li>
+                                <li class="list-group-item"><strong>Teléfono:</strong> ${usuario.phone}</li>
+                                <li class="list-group-item"><strong>Compañía:</strong> ${usuario.company.name}</li>
+                            </ul>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">${usuario.website}</small>
                         </div>
                     </div>
-                </div></div>`
+                </div>`
     }
     card += "</div>"
 
